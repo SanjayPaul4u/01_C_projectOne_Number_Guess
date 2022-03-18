23c2a5d47076d433709528e118863144e727f21a
SanjayPaul4u <sanjoypaul655@gmail.com>
2022-03-18 12:46:03 +0530

    For game all code is in game.exe

---
+++ game.c
@@ -0,0 +1,36 @@
+# include<stdio.h>
+# include<stdlib.h>
+# include<time.h>
+
+int main(){
+    int randNum, userNum, numberOfGuesses=1;
+
+    srand(time(0));
+    randNum= rand()%100+1;
+    // printf("The random number is %d\n", randNum);
+    
+    
+    
+    do
+    {
+        printf("Enter numbern between 1 to 100:\n");
+        scanf("%d", &userNum);
+
+        if(userNum<randNum){
+            printf("Heigher number please\n");
+        }
+        else if(userNum>randNum){
+            printf("Lower number please\n");
+        }
+        else{
+            printf("*******You guesses the number in %d attempts.\n", numberOfGuesses);
+        }
+
+        numberOfGuesses++;
+    } while (userNum!=randNum);
+
+    printf("--------------------The random number is %d------------------------- \n", randNum);
+    printf("-----------------And your guesses number is %d------------------ \n", userNum);
+
+    return 0;
+};
